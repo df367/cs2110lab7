5b86f1d1f03feb46290ca5f2ebefdbdfedc9062b
Diana <owner@dhcp-vl2042-14332.redrover.cornell.edu>
2022-07-27 16:03:26 -0400

    Added the author's book in Book object in file Book.java and described the changes done in README.md

--- Book.java
+++ Book.java
@@ -1,20 +1,27 @@
 /** A class modeling a book. */
 public class Book {
     String title;
+    String author;
+
 
     /** Create a new book with title b. */
-    public Book(String t) {
+    public Book(String t, String a) {
         title= t;
+        author = a;
     }
+
+    /** Create a new book with author. */
+
 
     /** Print this book. */
     public void printBook() {
         System.out.println(title);
+        System.out.println(author);
     }
 
     /** Some book examples. */
     public static void main(String[] args) {
-        Book b = new Book("A Clockwork Orange");
+        Book b = new Book("A Clockwork Orange", "Anthony Burgess");
         b.printBook();
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 An example git repository for CS 2110.
+The Book object contains the book's title and author, in this case the title is "A Clockwork Orange" and the author is "Anthony Burgess".
